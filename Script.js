ad9d5932286bfd5b1c639fc180cb08c6d26418dc
deerty666 <alraiee318@gmail.com>
2026-01-02 16:39:46 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -451,20 +451,18 @@ const menuData = [
             ]},
             // الوجبة 8
             {
-  id:"app-mshkl",
-  img:"/Dirty55/ap08.webp",
-  name:"مشكل مقبلات",
-  basePrice: 13,   // السعر الأصلي
-  branchDiscounts: {
-    'branch1': 10,
-    'branch2': 10,
-    'branch3': 10
-  },
-  availableIn: ['branch1', 'branch2', 'branch3'],
-  options:[
-    {name:"طبق", price:0}
-  ],
-},
+                id:"t5", 
+                img:"/Dirty55/ap08.webp", 
+                name:"مشكل مقبلات", 
+                basePrice:13, 
+                isBestSeller: true, 
+                branchDiscounts: { 
+                    'branch1': 10,
+                    'branch2': 10,
+                    'branch3': 10
+                },
+                
+
             {id:"side5", img:"/Dirty55/si03.webp", name:"طحينه", basePrice:3, availableIn: ['branch1', 'branch2', 'branch3'], options:[{name:"علبه", price:0}]},
             // الوجبة 4
             {id:"side6", img:"/Dirty55/si04.webp", name:"سلطه حار", basePrice:3, availableIn: ['branch1', 'branch2', 'branch3'], options:[{name:"علبه", price:0}]},
